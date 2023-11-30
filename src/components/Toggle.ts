a5c7fa57cde0c914c690b0eeef9fe551620789c5
Edward Tanguay (windows) <edward@tanguay.info>
2023-12-01 00:05:17 +0100

    dynamic

--- src/components/Toggle.ts
+++ src/components/Toggle.ts
@@ -1,10 +1,31 @@
-export const Toggle = (idCode: string, shape = 'round') => {
+export const Toggle = (idCode: string, onText = 'on', offText = 'off', shape = "round") => {
+	setTimeout(() => {
+		const checkboxElem = document.querySelector<HTMLDivElement>(
+			`.toggle_${idCode} input`
+		);
+		const messageElem = document.querySelector<HTMLDivElement>(
+			`.toggle_${idCode} p`
+		);
+
+		if (checkboxElem && messageElem) {
+			checkboxElem.addEventListener("change", (e: Event) => {
+				const elem = e.target as HTMLInputElement;
+				if (elem.checked) {
+					messageElem.innerText = onText;
+				} else {
+					messageElem.innerText = offText;
+				}
+			})
+		}
+	}, 0);
+
 	return /*html*/ `
-	<div class="toggleSwitch mb-3">
+	<div class="toggleSwitch mb-3 toggle_${idCode} flex gap-4">
 		<label class="switch">
 			<input type="checkbox">
-			<span class="slider ${shape === 'round' ? 'round' : ''}"></span>
+			<span class="slider ${shape === "round" ? "round" : ""}"></span>
 		</label>
+		<p class="text-3xl -mt-1 message">${offText}</p>
 	</div>
 	`;
 };
--- src/pages/PageToggle.ts
+++ src/pages/PageToggle.ts
@@ -2,9 +2,24 @@ import { Toggle } from "../components/Toggle";
 import { Wrapper } from "../components/Wrapper";
 
 export const PageToggle = () => {
-	return /*html*/`
+	return /*html*/ `
 <div>
-	${Wrapper('First toggle', Toggle('001', 'square') + Toggle('002'))}
+	${Wrapper(
+		"Default Toggle",
+		Toggle("001")
+	)}
+	${Wrapper(
+		"Square Toggle",
+		Toggle("002", 'on', 'off', 'square')
+	)}
+	${Wrapper(
+		"Custom Toggle #1",
+		Toggle("003", "online", "offline", "square") 
+	)}
+	${Wrapper(
+		"Custom Toggle #2",
+		Toggle("004", "I accept the terms of the contract.", "I do not accept the terms of the contract.", "round") 
+	)}
 </div>
 `;
-}
+};
